af8ee70fb7b8691e9b205006bcf91afc04dd1fc2
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-07-07 11:45:00 +0700

    Cập nhật tạm thời
    Chưa xong được :-1:

--- src/app/bravo-checklist/bravo-checklist.component.ts
+++ src/app/bravo-checklist/bravo-checklist.component.ts
@@ -1,15 +1,102 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  Injector,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Control } from '@grapecity/wijmo';
+import { WjDirectiveBehavior } from '@grapecity/wijmo.angular2.directivebase';
+import { BravoGraphicsRenderer } from '../bravo-graphics/bravo.graphics.renderer';
+import { Font } from '../bravo-graphics/font';
+
+import * as wjc from '@grapecity/wijmo';
 
 @Component({
-  selector: 'app-bravo-checklist',
+  selector: 'bravo-checklist',
   templateUrl: './bravo-checklist.component.html',
-  styleUrls: ['./bravo-checklist.component.css']
+  styleUrls: ['./bravo-checklist.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: BravoChecklistComponent,
+      multi: true,
+    },
+  ],
 })
-export class BravoChecklistComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class BravoChecklistComponent
+  extends Control
+  implements OnInit, ControlValueAccessor
+{
+  private _valueList!: string[];
+  public set valueList(pzValueList: string[]) {
+    this._valueList = pzValueList;
+  }
+  public get valueList(): string[] {
+    return this._valueList;
   }
 
+  private _zSeparator!: string;
+  public set zSeparator(pzValue: string) {
+    this._zSeparator = pzValue;
+  }
+  public get zSeparator(): string {
+    return this._zSeparator;
+  }
+
+  private _zText!: string;
+  public set zText(pzValue: string) {
+    this._zText = pzValue;
+  }
+  public get zText(): string {
+    return this._zText;
+  }
+
+  private _bAllowSelectMultiValue!: boolean;
+  public set bAllowSelectMultiValue(pbValue: boolean) {
+    this._bAllowSelectMultiValue = pbValue;
+  }
+  public get bAllowSelectMultiValue(): boolean {
+    return this._bAllowSelectMultiValue;
+  }
+
+  private _listType!: ListType;
+  public set listType(pnValue: ListType) {
+    this._listType = pnValue;
+  }
+  public get listType(): ListType {
+    return this._listType;
+  }
+
+  private _controls!: wjc.ObservableArray;
+  public set controls(pValue: wjc.ObservableArray) {
+    this._controls = pValue;
+  }
+  public get controls(): wjc.ObservableArray {
+    return this._controls;
+  }
+
+  constructor(
+    @Inject(ElementRef) private elRef: ElementRef,
+    @Inject(Injector) injector: Injector
+  ) {
+    super(WjDirectiveBehavior.getHostElement(elRef, injector));
+  }
+
+  writeValue(obj: any): void {}
+  registerOnChange(fn: any): void {}
+  registerOnTouched(fn: any): void {}
+
+  public override refresh(fullUpdate?: boolean) {
+    super.refresh(fullUpdate);
+  }
+
+  ngOnInit(): void {}
+}
+
+enum ListType {
+  Checkbox,
+  Button,
 }
