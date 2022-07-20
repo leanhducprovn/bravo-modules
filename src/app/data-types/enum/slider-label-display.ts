14829793bd7160e6508b266d72ac8fec79d0b157
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-07-20 15:47:34 +0700

    Khai báo enum
    Enum slider

---
+++ src/app/data-types/enum/slider-label-display.ts
@@ -0,0 +1,8 @@
+export enum SliderLabelDisplay {
+  // Không hiển thị
+  None = 0,
+  // Hiển thị ở vị trí min, max
+  MinMax = 1,
+  // Hiển thị ở vị trí tick
+  Tick = 2,
+}
---
+++ src/app/data-types/enum/slider-label-position.ts
@@ -0,0 +1,6 @@
+export enum SliderLabelPosition {
+  // Hiển thị phía trên
+  Above = 0,
+  // Hiển thị phía dưới
+  Below = 1,
+}
