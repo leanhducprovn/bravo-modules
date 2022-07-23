a2ea28476caa9d5389adde13b98a0bd6205e5610
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-07-23 09:34:01 +0700

    Cập nhật slider base
    Hừm

--- src/app/bravo-slider-base/bravo-slider-base.component.ts
+++ src/app/bravo-slider-base/bravo-slider-base.component.ts
@@ -1,18 +1,55 @@
 import { Options } from '@angular-slider/ngx-slider';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+
+// wijmo
+import * as wjc from '@grapecity/wijmo';
+
+// enum
+import { SliderTickStyle } from '../data-types/enum/slider-tick-style';
+import { SliderLabelDisplay } from '../data-types/enum/slider-label-display';
+import { SliderLabelPosition } from '../data-types/enum/slider-label-position';
 
 @Component({
   selector: 'bravo-slider-base',
   templateUrl: './bravo-slider-base.component.html',
   styleUrls: ['./bravo-slider-base.component.css'],
 })
-export class BravoSliderBaseComponent implements OnInit {
-  options: Options = {
+export class BravoSliderBaseComponent extends wjc.Control implements OnInit {
+  public options: Options = {
     floor: 0,
-    ceil: 250,
+    ceil: 100,
+    tickStep: 10,
+    showTicks: true,
+    ticksArray: undefined,
+    showSelectionBar: true,
+    getLegend: this.getLegend,
   };
 
-  constructor() {}
+  public constructor(elementRef: ElementRef) {
+    super(elementRef.nativeElement);
+  }
 
-  ngOnInit(): void {}
+  public override refresh(fullUpdate?: boolean): void {
+    super.refresh(fullUpdate);
+  }
+
+  public ngOnInit(): void {}
+
+  public getLegend(value: number): string {
+    return `${value}`;
+  }
+
+  private getCollection(...className: Array<string>) {
+    const _elements = new Array<HTMLElement>();
+    for (const zClassName of className) {
+      _elements.push(
+        ...Array.from(
+          this.hostElement?.getElementsByClassName(
+            zClassName
+          ) as HTMLCollectionOf<HTMLElement>
+        )
+      );
+    }
+    return _elements;
+  }
 }
