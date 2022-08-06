c1e247b781c2ebb5bbfd66a4ccd5aed321bc9a5f
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-08-06 08:16:04 +0700

    Cập nhật admin class
    Ok :+1:

---
+++ src/app/class/Admin.ts
@@ -0,0 +1,15 @@
+export class Developer {
+  public name: string = 'Le Anh Duc';
+  public age: number = 18;
+  public gender: string = 'Male';
+  public address: string = 'Ha Noi, Viet Nam';
+  public skills: string[] = [
+    'Python',
+    'C/C++/C#',
+    'HTML/CSS/JavaScript',
+    'Angular',
+    'TypeScript',
+    'NodeJS',
+    'Mongodb',
+  ];
+}
