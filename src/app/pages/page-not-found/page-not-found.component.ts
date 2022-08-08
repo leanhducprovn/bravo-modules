dac3f3ca89899156d96acedc2a0fea13d4004899
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-08-08 16:40:00 +0700

    Cập nhật trang 404
    Chưa hoàn thành :+1:

--- src/app/pages/page-not-found/page-not-found.component.ts
+++ src/app/pages/page-not-found/page-not-found.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.css']
+  styleUrls: ['./page-not-found.component.scss'],
 })
 export class PageNotFoundComponent implements OnInit {
-
-  constructor() { }
+  public content: string = '';
+  public arrContent: string[] = [];
+  constructor() {}
 
   ngOnInit(): void {
+    const possible =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    setInterval(() => {
+      this.arrContent.push(
+        possible.charAt(Math.floor(Math.random() * possible.length))
+      );
+    });
   }
-
 }
