8482a50f789e2a4528893599e5bccf3c3bd53af9
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-08-15 17:02:50 +0700

    Cập nhật Popup
    Hoàn thành 80% :+1:

--- src/app/components/bravo-picture-input-box/bravo-picture-input-box.component.ts
+++ src/app/components/bravo-picture-input-box/bravo-picture-input-box.component.ts
@@ -11,20 +11,24 @@ export class BravoPictureInputBoxComponent
   extends wjc.Control
   implements OnInit
 {
-  private _popup!: input.Popup;
+  @ViewChild('popup', { static: true }) private _popup!: input.Popup;
 
   constructor(elementRef: ElementRef) {
     super(elementRef.nativeElement);
   }
 
   public ngOnInit(): void {
-    this._popup = new input.Popup(
-      this.hostElement.querySelector('.bravo-picture-popup')
-    );
+    wjc.setCss(this.hostElement.querySelector('.bravo-picture-display'), {
+      width: '300px',
+      height: '160px',
+      borderRadius: 'unset',
+    });
     this._popup.owner = this.hostElement.querySelector(
       '.bravo-picture-dropdown'
     );
     this._popup.showTrigger = input.PopupTrigger.ClickOwner;
-    this._popup.hideTrigger = input.PopupTrigger.ClickOrBlur;
+    this._popup.hideTrigger =
+      input.PopupTrigger.Blur | input.PopupTrigger.ClickOwner;
+    this._popup.position = wjc.PopupPosition.BelowRight;
   }
 }
