bec6e4718b27192b31c2da64a15a821ba822a843
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-08-15 14:23:22 +0700

    Cập nhật giao diện chính
    Ok 90% :+1:

--- src/app/components/bravo-picture-input-box/bravo-picture-input-box.component.ts
+++ src/app/components/bravo-picture-input-box/bravo-picture-input-box.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as input from '@grapecity/wijmo.input';
+import * as wjc from '@grapecity/wijmo';
 
 @Component({
   selector: 'bravo-picture-input-box',
   templateUrl: './bravo-picture-input-box.component.html',
-  styleUrls: ['./bravo-picture-input-box.component.css'],
+  styleUrls: ['./bravo-picture-input-box.component.scss'],
 })
-export class BravoPictureInputBoxComponent implements OnInit {
-  constructor() {}
+export class BravoPictureInputBoxComponent
+  extends wjc.Control
+  implements OnInit
+{
+  @ViewChild('popup', { static: true }) popup!: input.Popup;
 
-  ngOnInit(): void {}
+  constructor(elementRef: ElementRef) {
+    super(elementRef.nativeElement);
+  }
+
+  ngOnInit(): void {
+    this.setPopup();
+  }
+
+  private setPopup() {
+    this.popup.showTrigger = input.PopupTrigger.ClickOwner;
+    this.popup.hideTrigger =
+      input.PopupTrigger.ClickOwner || input.PopupTrigger.LeavePopup;
+  }
 }
