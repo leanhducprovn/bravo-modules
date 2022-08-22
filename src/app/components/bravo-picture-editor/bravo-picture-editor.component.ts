e78c8365eeb24795fc8fcdc73411954b369d1da9
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-08-22 10:56:43 +0700

    Cập nhật linh tinh
    Haizzz :+1:

--- src/app/components/bravo-picture-editor/bravo-picture-editor.component.ts
+++ src/app/components/bravo-picture-editor/bravo-picture-editor.component.ts
@@ -13,8 +13,28 @@ import { Convert } from '../../library/bravo-convert/convert';
 export class BravoPictureEditorComponent extends wjc.Control implements OnInit {
   private _popup!: input.Popup;
 
-  public isZoom: boolean = false;
+  private _isZoom: boolean = false;
+  public set isZoom(pValue: boolean) {
+    if (this._isZoom == pValue) return;
+    this._isZoom = pValue;
+    this.invalidate();
+  }
+  public get isZoom(): boolean {
+    return this._isZoom;
+  }
+
+  private _isEdit: boolean = false;
+  public set isEdit(pValue: boolean) {
+    if (this._isEdit == pValue) return;
+    this._isEdit = pValue;
+    this.invalidate();
+  }
+  public get isEdit(): boolean {
+    return this._isEdit;
+  }
+
   public isPopup: boolean = false;
+  public editStyle!: string;
 
   constructor(elementRef: ElementRef) {
     super(elementRef.nativeElement);
@@ -22,10 +42,60 @@ export class BravoPictureEditorComponent extends wjc.Control implements OnInit {
 
   public override refresh(fullUpdate?: boolean | undefined): void {
     super.refresh(fullUpdate);
+    this.renderUI();
   }
 
   public ngOnInit(): void {
     this.setPopup();
+  }
+
+  public onZoom() {
+    this.isZoom = !this.isZoom;
+    this.isEdit = false;
+  }
+
+  public onEdit(style: string) {
+    this.isEdit = !this.isEdit;
+    this.isZoom = false;
+    this.editStyle = style;
+  }
+
+  private renderUI() {
+    let _view = this.hostElement?.querySelector('.bravo-picture-view');
+    let _preview = this.hostElement?.querySelector('.bravo-picture-preview');
+    let _edit = this.hostElement?.querySelector('.bravo-picture-edit');
+
+    // onZoom
+    if (_preview) {
+      if (this.isZoom) {
+        wjc.setCss(_preview, {
+          width: 'calc(100% - 40px)',
+        });
+      } else {
+        wjc.setCss(_preview, {
+          width: '100%',
+        });
+      }
+    }
+
+    // onBrightness
+    if (_view && _edit) {
+      if (this.isEdit) {
+        wjc.setCss(_edit, {
+          width: '100px',
+        });
+        wjc.setCss(_view, {
+          width: 'calc(100% - 100px)',
+        });
+      } else {
+        wjc.setCss(_edit, {
+          width: '0px',
+        });
+        wjc.setCss(_view, {
+          width: '100%',
+        });
+      }
+    }
   }
 
   private setPopup() {
