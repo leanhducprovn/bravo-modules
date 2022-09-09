c9587446db5bcaadc3c4c7c546dd696df165147d
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-09-09 16:23:51 +0700

    Cập nhật linh tinh
    Hmm :+1:

--- src/app/class/BravoToolbar.ts
+++ src/app/class/BravoToolbar.ts
@@ -1,8 +1,12 @@
 import * as wjc from '@grapecity/wijmo';
-import ResizeObserver from 'resize-observer-polyfill';
+// import ResizeObserver from 'resize-observer-polyfill';
+
+// const ResizeObserverPolyfill = require('resize-observer-polyfill');
+// const observer: ResizeObserver = new ResizeObserverPolyfill();
 
 export class BravoToolbal extends wjc.Control {
-  constructor(hostElement: HTMLElement) {
+  constructor(hostElement: HTMLElement | null) {
     super(hostElement);
+    console.log(hostElement);
   }
 }
--- src/app/components/bravo-test/bravo-test.component.ts
+++ src/app/components/bravo-test/bravo-test.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
+import * as wjc from '@grapecity/wijmo';
+
+import { BravoToolbal } from '../../class/BravoToolbar';
 @Component({
   selector: 'app-bravo-test',
   templateUrl: './bravo-test.component.html',
-  styleUrls: ['./bravo-test.component.css']
+  styleUrls: ['./bravo-test.component.scss'],
 })
-export class BravoTestComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class BravoTestComponent extends wjc.Control implements OnInit {
+  public constructor(elementRef: ElementRef) {
+    super(elementRef.nativeElement);
   }
 
+  ngOnInit(): void {
+    this.setToolbar();
+  }
+  private setToolbar() {
+    new BravoToolbal(this.hostElement.querySelector('.toolbar'));
+  }
 }
