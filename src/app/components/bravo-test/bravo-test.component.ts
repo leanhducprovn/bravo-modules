4fe51369394bce68b0e1788f20b414fa2ebd0f6b
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-09-09 13:24:49 +0700

    Khởi tạo Bravo Test
    Hoàn thành :100:

---
+++ src/app/components/bravo-test/bravo-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bravo-test',
+  templateUrl: './bravo-test.component.html',
+  styleUrls: ['./bravo-test.component.css']
+})
+export class BravoTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
