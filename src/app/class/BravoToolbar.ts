9e2f42e208884a0a63465bd0b0b8beb1e2558101
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-09-09 10:09:39 +0700

    Cập nhật linh tinh :+1:

--- src/app/class/BravoToolbar.ts
+++ src/app/class/BravoToolbar.ts
@@ -1,2 +1,8 @@
 import * as wjc from '@grapecity/wijmo';
 import ResizeObserver from 'resize-observer-polyfill';
+
+export class BravoToolbal extends wjc.Control {
+  constructor(hostElement: HTMLElement) {
+    super(hostElement);
+  }
+}
