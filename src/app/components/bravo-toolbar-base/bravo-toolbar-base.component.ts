b492dbf5828e171462d26b9fce3990597bc855f9
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-09-12 08:41:41 +0700

    Khởi tạo bravo toolbar base

---
+++ src/app/components/bravo-toolbar-base/bravo-toolbar-base.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bravo-toolbar-base',
+  templateUrl: './bravo-toolbar-base.component.html',
+  styleUrls: ['./bravo-toolbar-base.component.scss'],
+})
+export class BravoToolbarBaseComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
