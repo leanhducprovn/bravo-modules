a17403408d5bb9763de28123649246877db5b3ac
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-09-23 16:20:39 +0700

    Xóa bản quyền Wijmo
    Hoàn thành :100:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import * as wjc from '@grapecity/wijmo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent extends wjc.Control {
+  constructor(elementRef: ElementRef) {
+    super(elementRef.nativeElement);
+    let body = document.querySelector('body') as any
+    wjc.removeChild(body.lastChild)
+  }
+}
